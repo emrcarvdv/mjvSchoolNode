783cfc04b0522fe3721737e222b90304fc8d42e4
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2023-03-18 20:37:35 -0300

    Initial commit

---
+++ week2/hello-world.js
@@ -0,0 +1,10 @@
+console.log("Meu primeiro arquivo javascript no node!");
+
+let numberA = 2;
+let numberB = 2;
+
+const sum = (numA, numB) => {
+  return numA + numB;
+};
+
+console.log(`${numberA} + ${numberB} =`, sum(numberA, numberB));
