c6bf4d5a23646e85abf2cecc0db7e582614e9c34
Emerson Carvalho <emersoncarvalhodev@gmail.com>
2023-03-18 21:21:20 -0300

    exercicios aulas 02/03

---
+++ week2/first-project-node/primeiro-arquivo-typescript.ts
@@ -0,0 +1,8 @@
+console.log("Primeiro arquivo typescript!");
+
+let numberA: number = 2;
+let numberB: number = 2;
+
+let sum = (numA: number, numB: number) => numA + numB;
+
+console.log(`${numberA} + ${numberB} =`, sum(numberA, numberB));
